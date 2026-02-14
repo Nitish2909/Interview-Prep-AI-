e8cccd84c93f8e1e25ecbe139f9582127f082177
Nitish2909 <nitishraj7043@gmail.com>
2026-02-14 15:52:10 +0530

    updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server:{
+    allowedHosts:['interview-prep-ai-frontend-elw6.onrender.com']
+  }
 })
